74c7ecc128ab41ed87939a43c86b1916c3fdfde6
mwnCoding <MaximilianoNaddeo@gmail.com>
2023-09-12 15:18:33 +0200

    implement reset and split method

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -46,9 +46,11 @@ class Chronometer {
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
     // ... your code goes here
+    return this.computeTwoDigitNumber(this.getMinutes()) + ":" + this.computeTwoDigitNumber(this.getSeconds()) + "." + this.computeTwoDigitNumber(this.getCentiseconds());
   }
 }
