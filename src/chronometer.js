a5cd63d1f3ef382951c75b9705617c93e77d26ab
mwnCoding <MaximilianoNaddeo@gmail.com>
2023-09-12 13:55:06 +0200

    Iteration 8 done

--- src/chronometer.js
+++ src/chronometer.js
@@ -63,9 +63,6 @@ class Chronometer {
 
   split() {
     // ... your code goes here
+    return this.computeTwoDigitNumber(this.getMinutes()) + ":" + this.computeTwoDigitNumber(this.getSeconds());
   }
 }
-
-const chrono = new Chronometer();
-chrono.start();
-chrono.stop();
